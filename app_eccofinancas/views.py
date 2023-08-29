2f573a5e9ea039aad4fb4491105c4c895120353e
eldermarcos <eldermarcos17@gmail.com>
2023-08-28 23:40:56 -0300

    Como um usuário, eu preciso fazer login no sistema

--- app_eccofinancas/views.py
+++ app_eccofinancas/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def cadastro(request):
     if request.method == 'POST':
         email = request.POST.get('email')
-        #pegando o 'exemplo' do email "exemplo@email.com"
-        username = email.split('@')[0]
         first_name = request.POST.get('name')
         last_name = request.POST.get('lastname')
         password = request.POST.get('password')
  
         user = User.objects.create_user(email=email, 
-                                        username=username, 
+                                        username=email, 
                                         first_name=first_name,
                                         last_name=last_name, 
                                         password=password)
         user.save()
         #Assim que salvar o usuario novo, logar no sistema
-        login(request,user)
+        login(request, user)
         return redirect('/')
     return render(request,"cadastro.html")
+
+def conectar(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        usuario = authenticate(username=email, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else: 
+            messages.error(request, "Usuário ou senha inválidos")
+    return render(request,'login.html')
 
 def home(request):
     usuario = request.user
--- project_eccofinancas/urls.py
+++ project_eccofinancas/urls.py
@@ -21,5 +21,6 @@ from app_eccofinancas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.conectar, name='conectar'),
     path('', views.home, name='home'),
 ]
